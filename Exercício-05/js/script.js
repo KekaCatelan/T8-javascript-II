2d6b228ff3b6ba1e9471a7d4e97cfb2a9cb86103
kekacatelan <raquelpermarcat@outlook.com>
2019-09-27 16:44:13 -0300

    terminei o exercicio 05 e a metade do 06

--- Exercício-05/js/script.js
+++ Exercício-05/js/script.js
@@ -16,19 +16,71 @@ const livros = {
     }
 }
 
+const naveMae = document.getElementById('lista-do-catalogo')
+
 for(let propriedade in livros){
-    const naveMae = document.getElementById('lista-do-catalogo')
+    const meuLivro  = document.createElement('li')
+    naveMae.appendChild(meuLivro)
+    meuLivro.classList.add('livro')
+
     const tituloLivro = document.createElement('h2')
-    naveMae.appendChild(tituloLivro)
+    meuLivro.appendChild(tituloLivro)
     tituloLivro.textContent= livros[propriedade]['titulo']
+    tituloLivro.classList.add('livro__titulo')
+   
     const autor= document.createElement('p')
-    naveMae.appendChild(autor)
+    meuLivro.appendChild(autor)
     autor.textContent= livros[propriedade]['quemEscreveu']
+    autor.classList.add('livro__autoria')
+
     const link = document.createElement('a')
-    naveMae.appendChild(link)
+    meuLivro.appendChild(link)
     link.setAttribute('href' , livros[propriedade]['link'])
     link.setAttribute('target' , '_blank')
     link.textContent = 'Leia Aqui'
+    link.classList.add('livro__link')
+
+    const botao = document.createElement("BUTTON");
+    meuLivro.appendChild(botao)
+    botao.textContent = 'Marcar como lido'
+    botao.classList.add('botao-lido')
+
+    // const botaoCancelar = document.createElement("BUTTON")
+    // meuLivro.appendChild(botaoCancelar)
+    // botaoCancelar.textContent= 'Desfazer'
+    // botaoCancelar.classList.add('botao-desfazer')
+
+    botao.addEventListener('click' , function(evento) {
+        
+    if(tituloLivro.classList.contains('livro__titulo--lido')){
+        tituloLivro.classList.remove('livro__titulo--lido')
+        botao.textContent='Marcar como lido'
+        botao.classList.add('botao-lido')
+
+        botao.classList.remove('botao-lido__vermelho')
+        
+        
+    }else{
+        tituloLivro.classList.add('livro__titulo--lido')
+        botao.textContent='Lido'
+        botao.classList.add('botao-lido__vermelho')
+
+        
+    }
+    
+    })
+
+    // botaoCancelar.addEventListener('click' , function(evento){
+    //     if(tituloLivro.classList.contains('livro__titulo--lido')){
+    //         tituloLivro.classList.remove('livro__titulo--lido')
+    //         botao.textContent= 'Já Li'
+    //         botao.classList.remove('botao-lido__vermelho')
+        
+    //     }else{
+    //         tituloLivro.classList.add('livro__titulo--lido') 
+           
+    //     }
+    // })
 
 }
 
